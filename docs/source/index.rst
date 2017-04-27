48700cd34863dd354d9937047af77c1beec3d751
Leandro N <leandron85@gmail.com>
2017-04-27 20:28:04 +0100

    Improve documentation and provides a simple example

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -22,3 +22,27 @@ Installing
 It is easy to obtain the latest released version via ``pip``::
 
     pip install steinlib
+
+Usage
+=====
+
+In order to use the parsed structures, two main components are required:
+ - A *parser*, that will be responsible for parsing the structure, and;
+ - A *instance*, where the parser will invoke callbacks when known structures are found
+
+So, let's see an example about how it works.
+
+Starting with an STP file available on Steinlib official website:
+
+ .. literalinclude:: ../../examples/hello.stp
+
+It is possible to observe this file contains three main components:
+ - a **header line**;
+ - a set of delimited **sections** and;
+ - the **end of file** marker.
+
+This *parser* will identify all these small pieces of the STP file and use it to
+trigger functions on your *instance*. This is the key concept used to create
+this module.
+
+
---
+++ examples/hello_steinlib.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from steinlib.instance import SteinlibInstance
+from steinlib.parser import SteinlibParser
+
+import sys
+
+
+class MySteinlibInstance(SteinlibInstance):
+    """
+    This is my first steinlib parser!
+    """
+
+    def comment(self, raw_args, lis_args):
+        print "Comment section found"
+
+    def coordinates(self, raw_args, list_args):
+        print "Coordinates section found"
+
+    def eof(self, raw_args, list_args):
+        print "End of file found"
+
+    def graph(self, raw_args, list_args):
+        print "Graph section found"
+
+    def header(self, raw_args, list_args):
+        print "Header found"
+
+    def terminals(self, raw_args, list_args):
+        print "Terminals section found"
+
+
+if __name__ == "__main__":
+    my_class = MySteinlibInstance()
+    with open(sys.argv[1]) as my_file:
+        my_parser = SteinlibParser(my_file, my_class)
+        my_parser.parse()
